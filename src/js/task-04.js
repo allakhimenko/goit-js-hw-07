0e75fe0feb0f9c8c5d6413365fb8040df76365cf
allakhimenko <all.khimenko24@gmail.com>
2021-06-02 11:59:41 +0300

    задание4

---
+++ src/js/task-04.js
@@ -0,0 +1,8 @@
+const counterValue = document.querySelector('#value');
+const increment = document.querySelector('#increment');
+const decrement = document.querySelector('#decrement');
+
+counterValue.addEventListener('click', increment);
+counterValue.addEventListener('click', decrement)
+
+
